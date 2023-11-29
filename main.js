303053c81b2a19e20205919c4ae1319d3de67888
DHANUSHs311003 <dhanushphoenix2003@gmail.com>
2023-11-29 21:29:03 +0530

    completed

--- main.js
+++
@@ -1,9 +0,0 @@
-function BankAccount (accountNumber,name,type,balance) {
-  this.accountNumber = accountNumber;
-  this.name = name;
-  this.type = type;
-  this.balance = balance;
-}
-
-let ac1 = new BankAccount(112345456,"swetha","savings",2356);
-console.log(ac1);
